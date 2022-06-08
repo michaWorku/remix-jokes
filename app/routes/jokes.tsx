fc331889d7c73a704142647e29215259dbfefc92
michaWorku <mikias27worku@gmail.com>
2022-06-08 11:11:31 +0300

    logout done

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,28 +1,48 @@
-import { Joke } from "@prisma/client";
-import { json, LinksFunction, LoaderFunction } from "@remix-run/node";
-import { Outlet, Link, useLoaderData } from "@remix-run/react";
+import type { User } from "@prisma/client";
+import type {
+  LinksFunction,
+  LoaderFunction,
+} from "@remix-run/node";
+import { json } from "@remix-run/node";
+import {
+  Link,
+  Outlet,
+  useLoaderData,
+} from "@remix-run/react";
 
-import stylesUrl from "~/styles/jokes.css";
 import { db } from "~/utils/db.server";
-
-type LoaderData= {jokeListItems : Array<Pick<Joke, | 'id' | 'name'>>}
-export let loader : LoaderFunction = async ()=>{
-    let jokeListItems = await db.joke.findMany({
-        take: 5,
-        select:{id:true, name: true},
-        orderBy: {createdAt: 'desc'}
-    })
-    let data : LoaderData = {jokeListItems}
-
-    return json(data)
-}
+import { getUser } from "~/utils/session.server";
+import stylesUrl from "~/styles/jokes.css";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: stylesUrl }];
 };
 
+type LoaderData = {
+  user: Awaited<ReturnType<typeof getUser>>;
+  jokeListItems: Array<{ id: string; name: string }>;
+};
+
+export const loader: LoaderFunction = async ({
+  request,
+}) => {
+  const jokeListItems = await db.joke.findMany({
+    take: 5,
+    orderBy: { createdAt: "desc" },
+    select: { id: true, name: true },
+  });
+  const user = await getUser(request);
+
+  const data: LoaderData = {
+    jokeListItems,
+    user,
+  };
+  return json(data);
+};
+
 export default function JokesRoute() {
-  let {jokeListItems} = useLoaderData<LoaderData>()
+  const data = useLoaderData<LoaderData>();
+
   return (
     <div className="jokes-layout">
       <header className="jokes-header">
@@ -37,6 +57,18 @@ export default function JokesRoute() {
               <span className="logo-medium">J🤪KES</span>
             </Link>
           </h1>
+          {data.user ? (
+            <div className="user-info">
+              <span>{`Hi ${data.user.username}`}</span>
+              <form action="/logout" method="post">
+                <button type="submit" className="button">
+                  Logout
+                </button>
+              </form>
+            </div>
+          ) : (
+            <Link to="/login">Login</Link>
+          )}
         </div>
       </header>
       <main className="jokes-main">
@@ -45,13 +77,11 @@ export default function JokesRoute() {
             <Link to=".">Get a random joke</Link>
             <p>Here are a few more jokes to check out:</p>
             <ul>
-              {
-                  jokeListItems.map((joke)=>(
-                    <li key={joke.id}>
-                        <Link to={joke.id}>{joke.name}</Link>
-                    </li>
-                  ))
-              }
+              {data.jokeListItems.map((joke) => (
+                <li key={joke.id}>
+                  <Link to={joke.id}>{joke.name}</Link>
+                </li>
+              ))}
             </ul>
             <Link to="new" className="button">
               Add your own
